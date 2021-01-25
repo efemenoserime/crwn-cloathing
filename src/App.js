d375eb4f139997e41bc4db348cc65d842db8ccdc
Efeme Noserime <noserimeefeme@gmail.com>
2021-01-25 02:45:48 +0100

    Install and utitize react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 import Homepage from "./pages/Homepage/Homepage.component";
 
 function App() {
   return (
     <div>
-      {" "}
-      <Homepage />{" "}
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
