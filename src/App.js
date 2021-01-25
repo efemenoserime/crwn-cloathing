405af762e4d1cd0626b13a32e15bcd934cb18a90
Efeme Noserime <noserimeefeme@gmail.com>
2021-01-25 02:12:58 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+import Homepage from "./pages/Homepage/Homepage.component";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <Homepage />{" "}
+    </div>
+  );
+}
+
+export default App;
