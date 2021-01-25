ea80cce0c53e54ac1eb08476e54e5111f6088ae2
Efeme Noserime <noserimeefeme@gmail.com>
2021-01-25 14:36:25 +0100

    feat: create header component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/Homepage.component";
 import ShopPage from "./pages/ShopPage/Shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/shop" component={ShopPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
