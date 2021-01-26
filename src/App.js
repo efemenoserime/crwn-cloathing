66e8f957b5d70fd4f89afda84a971b00d09b1f97
Efeme Noserime <noserimeefeme@gmail.com>
2021-01-26 19:51:33 +0100

    make app aware of auth state
    * App component to class-based component
    * implement method to unsubscribe from auth

--- src/App.js
+++ src/App.js
@@ -7,17 +7,43 @@ import ShopPage from "./pages/ShopPage/Shop.component";
 import Header from "./components/header/header.component";
 import SignPage from "./pages/SignPage/SignPage.component";
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/shop" component={ShopPage} />
-				<Route path="/signin" component={SignPage} />
-			</Switch>
-		</div>
-	);
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			currentUser: null,
+		};
+	}
+
+	unsubsribeFromAuth = null;
+
+	componentDidMount() {
+		this.unsubsribeFromAuth = auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user });
+
+			console.log(user);
+		});
+	}
+
+	componentWillMount() {
+		this.unsubsribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={SignPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
