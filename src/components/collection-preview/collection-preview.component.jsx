3947aed11a561ec4e723faa80ea58834e573bcc3
Efeme Noserime <noserimeefeme@gmail.com>
2021-02-10 17:59:54 +0100

    use redux in collection item & preview component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -5,18 +5,18 @@ import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
-  return (
-    <div className="collection-preview">
-      <h1>{title.toUpperCase()}</h1>
-      <div className="preview">
-        {items
-          .filter((item, index) => index < 4)
-          .map(({ id, ...itemProps }) => (
-            <CollectionItem key={id} {...itemProps} />
-          ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="collection-preview">
+			<h1>{title.toUpperCase()}</h1>
+			<div className="preview">
+				{items
+					.filter((item, index) => index < 4)
+					.map((item) => (
+						<CollectionItem key={item.id} item={item} />
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default CollectionPreview;
