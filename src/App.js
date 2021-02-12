43acf2379883eea1639e7b3171dcb5e66c9ec8cf
Efeme Noserime <noserimeefeme@gmail.com>
2021-02-12 18:30:33 +0100

    feat: add checkout page and use cart selectors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/Homepage.component";
 import ShopPage from "./pages/ShopPage/Shop.component";
 import Header from "./components/header/header.component";
 import SignPage from "./pages/SignPage/SignPage.component";
+import CheckoutPage from "./pages/CheckoutPage/Checkout.component";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selector";
 
 import "./App.css";
 
@@ -53,14 +55,15 @@ class App extends React.Component {
 						path="/signin"
 						render={() => (currentUser ? <Redirect to="/" /> : <SignPage />)}
 					/>
+					<Route exact path="/checkout" component={CheckoutPage} />
 				</Switch>
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = ({ user }) => ({
-	currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
