57f5aab87ef17a99dd04e08be8248f9631ef12af
Efeme Noserime <noserimeefeme@gmail.com>
2021-02-13 19:50:50 +0100

    move directory & shop date into onw reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,7 +14,6 @@ const Directory = ({ sections }) => {
 			{sections.map(({ id, ...otherSectionProps }) => (
 				<MenuItem key={id} {...otherSectionProps} />
 			))}
-			{console.log(sections[0])}
 		</div>
 	);
 };
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage";
 
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 import userReducer from "./user/user.reducer";
 
 const persistConfig = {
@@ -15,6 +16,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
 	cart: cartReducer,
 	directory: directoryReducer,
+	shop: shopReducer,
 	user: userReducer,
 });
 
