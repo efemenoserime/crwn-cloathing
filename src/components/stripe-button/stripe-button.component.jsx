28fd1c3563a171616f78b78cbc5ad97bea0590fa
Efeme Noserime <noserimeefeme@gmail.com>
2021-02-14 04:28:08 +0100

    feat: create stripe-checkout button

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = "pk_test_YygFctwfYKkMp5y7RQFOVPbw00bjyVpB3C";
+
+	const onToken = (token) => {
+		console.log(token);
+		alert("Payment Successful");
+	};
+
+	return (
+		<StripeCheckout
+			label="Pay now"
+			name="CRWN Cloathing KG"
+			billingAddress
+			shippingAddress
+			description={`Your total is $ ${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
